4b47281a360e2b7b939056ade049c37673447adf
Udhay rajeev <udhay1co.de@gmail.com>
2023-02-16 14:57:58 +0530

    Fix

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 export async function connectToDatabase(){
-    const client = await MongoClient.connect(process.env.MONGO_DB) // jsIxIsIvJYH5LTsp passw username: udhay
+    const client = await MongoClient.connect(process.env.MONGO_DB) 
 
     const db = client.db("landing")
 
